835a9fe311d6013ca101ab22d21301b555ab91d6
Karang <arthur.hennequin@free.fr>
2020-07-13 01:31:38 +0200

    Improve block rendering

---
+++ src/models.js
@@ -0,0 +1,206 @@
+/* global THREE */
+
+const Vec3 = require('vec3').Vec3
+
+const elemFaces = {
+  up: {
+    dir: new Vec3(0, 1, 0),
+    mask1: [1, 1, 0],
+    mask2: [0, 1, 1],
+    corners: [
+      [0, 1, 1, 0, 1],
+      [1, 1, 1, 1, 1],
+      [0, 1, 0, 0, 0],
+      [1, 1, 0, 1, 0]
+    ]
+  },
+  down: {
+    dir: new Vec3(0, -1, 0),
+    mask1: [1, 1, 0],
+    mask2: [0, 1, 1],
+    corners: [
+      [1, 0, 1, 0, 1],
+      [0, 0, 1, 1, 1],
+      [1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0]
+    ]
+  },
+  east: {
+    dir: new Vec3(1, 0, 0),
+    mask1: [1, 1, 0],
+    mask2: [1, 0, 1],
+    corners: [
+      [1, 1, 1, 0, 0],
+      [1, 0, 1, 0, 1],
+      [1, 1, 0, 1, 0],
+      [1, 0, 0, 1, 1]
+    ]
+  },
+  west: {
+    dir: new Vec3(-1, 0, 0),
+    mask1: [1, 1, 0],
+    mask2: [1, 0, 1],
+    corners: [
+      [0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 1],
+      [0, 1, 1, 1, 0],
+      [0, 0, 1, 1, 1]
+    ]
+  },
+  north: {
+    dir: new Vec3(0, 0, -1),
+    mask1: [1, 0, 1],
+    mask2: [0, 1, 1],
+    corners: [
+      [1, 0, 0, 0, 1],
+      [0, 0, 0, 1, 1],
+      [1, 1, 0, 0, 0],
+      [0, 1, 0, 1, 0]
+    ]
+  },
+  south: {
+    dir: new Vec3(0, 0, 1),
+    mask1: [1, 0, 1],
+    mask2: [0, 1, 1],
+    corners: [
+      [0, 0, 1, 0, 1],
+      [1, 0, 1, 1, 1],
+      [0, 1, 1, 0, 0],
+      [1, 1, 1, 1, 0]
+    ]
+  }
+}
+
+function isCube (shapes) {
+  if (shapes.length !== 1) return false
+  const shape = shapes[0]
+  return shape[0] === 0 && shape[1] === 0 && shape[2] === 0 && shape[3] === 1 && shape[4] === 1 && shape[5] === 1
+}
+
+function renderElement (world, cursor, element, doAO, attr) {
+  for (const face in element.faces) {
+    const eFace = element.faces[face]
+    const { dir, corners, mask1, mask2 } = elemFaces[face]
+
+    if (eFace.cullface) {
+      const neighbor = world.getBlock(cursor.plus(dir))
+      if (!neighbor || !(neighbor.transparent || !isCube(neighbor.shapes)) || neighbor.position.y < 0) continue
+    }
+
+    const minx = element.from[0] / 16
+    const miny = element.from[1] / 16
+    const minz = element.from[2] / 16
+    const maxx = element.to[0] / 16
+    const maxy = element.to[1] / 16
+    const maxz = element.to[2] / 16
+
+    const u = eFace.texture.u
+    const v = eFace.texture.v
+    const su = eFace.texture.su
+    const sv = eFace.texture.sv
+
+    const ndx = Math.floor(attr.positions.length / 3)
+
+    let tint = [1, 1, 1]
+    if (eFace.tintindex !== undefined) {
+      if (eFace.tintindex === 0) {
+        tint = [0.3, 1.0, 0.4] // TODO: correct tint for each block
+      }
+    }
+
+    const aos = []
+    for (const pos of corners) {
+      attr.positions.push((pos[0] ? maxx : minx) + cursor.x, (pos[1] ? maxy : miny) + cursor.y, (pos[2] ? maxz : minz) + cursor.z)
+      attr.normals.push(dir.x, dir.y, dir.z)
+      attr.uvs.push(pos[3] * su + u, pos[4] * sv + v)
+
+      let light = 1
+
+      if (doAO) {
+        const dx = pos[0] * 2 - 1
+        const dy = pos[1] * 2 - 1
+        const dz = pos[2] * 2 - 1
+        const side1 = world.getBlock(cursor.offset(dx * mask1[0], dy * mask1[1], dz * mask1[2]))
+        const side2 = world.getBlock(cursor.offset(dx * mask2[0], dy * mask2[1], dz * mask2[2]))
+        const corner = world.getBlock(cursor.offset(dx, dy, dz))
+
+        const side1Block = (side1 && isCube(side1.shapes)) ? 1 : 0
+        const side2Block = (side2 && isCube(side2.shapes)) ? 1 : 0
+        const cornerBlock = (corner && isCube(corner.shapes)) ? 1 : 0
+
+        // TODO: correctly interpolate ao light based on pos (evaluate once for each corner of the block)
+
+        const ao = (side1Block && side2Block) ? 0 : (3 - (side1Block + side2Block + cornerBlock))
+        light = ao / 3
+        aos.push(ao)
+      }
+
+      attr.colors.push(tint[0] * light, tint[1] * light, tint[2] * light)
+    }
+
+    if (doAO && aos[0] + aos[3] >= aos[1] + aos[2]) {
+      attr.indices.push(
+        ndx, ndx + 3, ndx + 2,
+        ndx, ndx + 1, ndx + 3
+      )
+    } else {
+      attr.indices.push(
+        ndx, ndx + 1, ndx + 2,
+        ndx + 2, ndx + 1, ndx + 3
+      )
+    }
+  }
+}
+
+function getSectionGeometry (sx, sy, sz, world) {
+  const attr = {
+    positions: [],
+    normals: [],
+    colors: [],
+    uvs: [],
+    indices: []
+  }
+
+  const cursor = new Vec3(0, 0, 0)
+  for (cursor.y = sy; cursor.y < sy + 16; cursor.y++) {
+    for (cursor.z = sz; cursor.z < sz + 16; cursor.z++) {
+      for (cursor.x = sx; cursor.x < sx + 16; cursor.x++) {
+        const block = world.getBlock(cursor)
+        const variant = getModelVariant(block, world.blocksStates)
+        if (!variant || !variant.model) continue
+
+        for (const element of variant.model.elements) {
+          renderElement(world, cursor, element, variant.model.ao, attr)
+        }
+      }
+    }
+  }
+
+  const geometry = new THREE.BufferGeometry()
+  geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(attr.positions), 3))
+  geometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(attr.normals), 3))
+  geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(attr.colors), 3))
+  geometry.setAttribute('uv', new THREE.BufferAttribute(new Float32Array(attr.uvs), 2))
+  geometry.setIndex(attr.indices)
+  return geometry
+}
+
+function getModelVariant (block, blockStates) {
+  const state = blockStates[block.name]
+  if (!state) return null
+  if (state.variants) {
+    for (const variant of Object.values(state.variants)) { // TODO: match properties
+      if (variant instanceof Array) return variant[0] // TODO: random
+      return variant
+    }
+  }
+  if (state.multipart) {
+    for (const variant of state.multipart) { // TODO: match properties
+      if (variant.apply instanceof Array) return variant.apply[0] // TODO: random
+      return variant.apply
+    }
+  }
+  return null
+}
+
+module.exports = { getSectionGeometry }
--- src/world.js
+++ src/world.js
@@ -1,75 +1,8 @@
-/* global THREE */
+/* global THREE XMLHttpRequest */
 
-const Vec3 = require('vec3').Vec3
+const { Vec3 } = require('vec3')
 
-const cubeFaces = [
-  { // left
-    dir: new Vec3(-1, 0, 0),
-    mask1: [1, 1, 0],
-    mask2: [1, 0, 1],
-    corners: [
-      [0, 1, 0],
-      [0, 0, 0],
-      [0, 1, 1],
-      [0, 0, 1]
-    ]
-  },
-  { // right
-    dir: new Vec3(1, 0, 0),
-    mask1: [1, 1, 0],
-    mask2: [1, 0, 1],
-    corners: [
-      [1, 1, 1],
-      [1, 0, 1],
-      [1, 1, 0],
-      [1, 0, 0]
-    ]
-  },
-  { // bottom
-    dir: new Vec3(0, -1, 0),
-    mask1: [1, 1, 0],
-    mask2: [0, 1, 1],
-    corners: [
-      [1, 0, 1],
-      [0, 0, 1],
-      [1, 0, 0],
-      [0, 0, 0]
-    ]
-  },
-  { // top
-    dir: new Vec3(0, 1, 0),
-    mask1: [1, 1, 0],
-    mask2: [0, 1, 1],
-    corners: [
-      [0, 1, 1],
-      [1, 1, 1],
-      [0, 1, 0],
-      [1, 1, 0]
-    ]
-  },
-  { // back
-    dir: new Vec3(0, 0, -1),
-    mask1: [1, 0, 1],
-    mask2: [0, 1, 1],
-    corners: [
-      [1, 0, 0],
-      [0, 0, 0],
-      [1, 1, 0],
-      [0, 1, 0]
-    ]
-  },
-  { // front
-    dir: new Vec3(0, 0, 1),
-    mask1: [1, 0, 1],
-    mask2: [0, 1, 1],
-    corners: [
-      [0, 0, 1],
-      [1, 0, 1],
-      [0, 1, 1],
-      [1, 1, 1]
-    ]
-  }
-]
+const { getSectionGeometry } = require('./models')
 
 function columnKey (x, z) {
   return `${x},${z}`
@@ -87,103 +20,19 @@ function posInChunk (pos) {
   return pos
 }
 
-function isCube (shapes) {
-  if (shapes.length !== 1) return false
-  const shape = shapes[0]
-  return shape[0] === 0 && shape[1] === 0 && shape[2] === 0 && shape[3] === 1 && shape[4] === 1 && shape[5] === 1
-}
-
-function addUvs (uvs, blockIndex, textureWidth) {
-  const tileS = 16 / textureWidth
-  const tileN = Math.floor(textureWidth / 16)
-  const x = (blockIndex % tileN) * tileS
-  const y = Math.floor(blockIndex / tileN) * tileS
-  uvs.push(x, y, x, y + tileS, x + tileS, y, x + tileS, y + tileS)
-}
-
-function getSectionGeometry (sx, sy, sz, world) {
-  const positions = []
-  const normals = []
-  const colors = []
-  const uvs = []
-  const indices = []
-
-  const cursor = new Vec3(0, 0, 0)
-  for (cursor.y = sy; cursor.y < sy + 16; cursor.y++) {
-    for (cursor.z = sz; cursor.z < sz + 16; cursor.z++) {
-      for (cursor.x = sx; cursor.x < sx + 16; cursor.x++) {
-        const block = world.getBlock(cursor)
-        if (block && block.shapes.length > 0) {
-          if (isCube(block.shapes)) {
-            for (const { dir, corners, mask1, mask2 } of cubeFaces) {
-              const neighbor = world.getBlock(cursor.plus(dir))
-              if (neighbor && !isCube(neighbor.shapes) && neighbor.position.y >= 0) {
-                const ndx = Math.floor(positions.length / 3)
-                const aos = []
-                for (const pos of corners) {
-                  positions.push(pos[0] + cursor.x, pos[1] + cursor.y, pos[2] + cursor.z)
-                  normals.push(dir.x, dir.y, dir.z)
-
-                  // Ambient occlusion
-                  const dx = pos[0] * 2 - 1
-                  const dy = pos[1] * 2 - 1
-                  const dz = pos[2] * 2 - 1
-                  const side1 = world.getBlock(cursor.offset(dx * mask1[0], dy * mask1[1], dz * mask1[2]))
-                  const side2 = world.getBlock(cursor.offset(dx * mask2[0], dy * mask2[1], dz * mask2[2]))
-                  const corner = world.getBlock(cursor.offset(dx, dy, dz))
-
-                  const side1Block = (side1 && isCube(side1.shapes)) ? 1 : 0
-                  const side2Block = (side2 && isCube(side2.shapes)) ? 1 : 0
-                  const cornerBlock = (corner && isCube(corner.shapes)) ? 1 : 0
-
-                  const ao = (side1Block && side2Block) ? 0 : (3 - (side1Block + side2Block + cornerBlock))
-                  aos.push(ao)
-
-                  colors.push(ao / 3, ao / 3, ao / 3)
-                }
-                addUvs(uvs, block.type, 400) // TODO: correct uvs
-                if (aos[0] + aos[3] < aos[1] + aos[2]) {
-                  indices.push(
-                    ndx, ndx + 1, ndx + 2,
-                    ndx + 2, ndx + 1, ndx + 3
-                  )
-                } else {
-                  indices.push(
-                    ndx, ndx + 3, ndx + 2,
-                    ndx, ndx + 1, ndx + 3
-                  )
-                }
-              }
-            }
-          } else {
-            for (const shape of block.shapes) {
-              for (const { dir, corners } of cubeFaces) {
-                const ndx = Math.floor(positions.length / 3)
-                for (const pos of corners) {
-                  positions.push((pos[0] ? shape[3] : shape[0]) + cursor.x, (pos[1] ? shape[4] : shape[1]) + cursor.y, (pos[2] ? shape[5] : shape[2]) + cursor.z)
-                  normals.push(dir.x, dir.y, dir.z)
-                  colors.push(1, 1, 1)
-                }
-                addUvs(uvs, block.type, 400) // TODO: correct uvs
-                indices.push(
-                  ndx, ndx + 1, ndx + 2,
-                  ndx + 2, ndx + 1, ndx + 3
-                )
-              }
-            }
-          }
-        }
-      }
+function getJSON (url, callback) {
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', url, true)
+  xhr.responseType = 'json'
+  xhr.onload = function () {
+    var status = xhr.status
+    if (status === 200) {
+      callback(null, xhr.response)
+    } else {
+      callback(status, xhr.response)
     }
   }
-
-  const geometry = new THREE.BufferGeometry()
-  geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), 3))
-  geometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(normals), 3))
-  geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3))
-  geometry.setAttribute('uv', new THREE.BufferAttribute(new Float32Array(uvs), 2))
-  geometry.setIndex(indices)
-  return geometry
+  xhr.send()
 }
 
 class World {
@@ -196,7 +45,13 @@ class World {
     texture.magFilter = THREE.NearestFilter
     texture.minFilter = THREE.NearestFilter
     texture.flipY = false
-    this.material = new THREE.MeshLambertMaterial({ map: texture, vertexColors: true })
+    this.material = new THREE.MeshLambertMaterial({ map: texture, vertexColors: true, transparent: true, alphaTest: 0.1 })
+
+    this.blocksStates = null
+    getJSON('blocksStates.json', (err, json) => {
+      if (err) return
+      this.blocksStates = json
+    })
   }
 
   setSectionDirty (pos) {
@@ -219,6 +74,8 @@ class World {
   }
 
   update () {
+    if (!this.blocksStates) return // not ready yet
+
     for (const coords in this.columns) {
       let [x, z] = coords.split(',')
       x = parseInt(x, 10)
