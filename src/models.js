5ad02444ae25d9062d3e77dc791aab080df936aa
Karang <arthur.hennequin@free.fr>
2020-07-13 21:08:32 +0200

    Correct block states and block rotation

--- src/models.js
+++ src/models.js
@@ -107,15 +107,21 @@ function renderElement (world, cursor, element, doAO, attr) {
         tint = [0.568, 0.741, 0.349] // TODO: correct tint for each block
       }
     }
+    // UV rotation
+    const r = eFace.rotation || 0
+    const uvcs = Math.cos(r * Math.PI / 180)
+    const uvsn = -Math.sin(r * Math.PI / 180)
 
     const aos = []
     for (const pos of corners) {
       attr.positions.push((pos[0] ? maxx : minx) + cursor.x, (pos[1] ? maxy : miny) + cursor.y, (pos[2] ? maxz : minz) + cursor.z)
       attr.normals.push(dir.x, dir.y, dir.z)
-      attr.uvs.push(pos[3] * su + u, pos[4] * sv + v)
+
+      const baseu = (pos[3] - 0.5) * uvcs - (pos[4] - 0.5) * uvsn + 0.5
+      const basev = (pos[3] - 0.5) * uvsn + (pos[4] - 0.5) * uvcs + 0.5
+      attr.uvs.push(baseu * su + u, basev * sv + v)
 
       let light = 1
-
       if (doAO) {
         const dx = pos[0] * 2 - 1
         const dy = pos[1] * 2 - 1
@@ -185,11 +191,32 @@ function getSectionGeometry (sx, sy, sz, world) {
   return geometry
 }
 
+function parseProperties (properties) {
+  const json = {}
+  for (const prop of properties.split(',')) {
+    const [key, value] = prop.split('=')
+    json[key] = value
+  }
+  return json
+}
+
+function matchProperties (block, properties) {
+  properties = parseProperties(properties)
+  const blockProps = block.getProperties()
+  for (const prop in blockProps) {
+    if (properties[prop] !== undefined && (blockProps[prop] + '') !== properties[prop]) {
+      return false
+    }
+  }
+  return true
+}
+
 function getModelVariant (block, blockStates) {
   const state = blockStates[block.name]
   if (!state) return null
   if (state.variants) {
-    for (const variant of Object.values(state.variants)) { // TODO: match properties
+    for (const [properties, variant] of Object.entries(state.variants)) {
+      if (!matchProperties(block, properties)) continue
       if (variant instanceof Array) return variant[0] // TODO: random
       return variant
     }
