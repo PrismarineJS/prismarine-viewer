5c5f51850a50acceabb089c2f637fcbd3c9b31f4
Romain Beaumont <romain.rom1@gmail.com>
2020-07-14 09:08:15 +0000

    configure puppeteer and gitpod

---
+++ test/simple.test.js
@@ -0,0 +1,12 @@
+/* eslint-env jest */
+/* global page */
+
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com')
+  })
+
+  it('should display "google" text on page', async () => {
+    await expect(page).toMatch('google')
+  })
+})
