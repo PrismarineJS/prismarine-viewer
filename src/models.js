2159ae30b42f047356d0ef9b119fb14f7c216e92
Karang <arthur.hennequin@free.fr>
2020-07-28 22:53:36 +0200

    Protect against undefined shapes

--- src/models.js
+++ src/models.js
@@ -70,7 +70,7 @@ const elemFaces = {
 }
 
 function isCube (shapes) {
-  if (shapes.length !== 1) return false
+  if (!shapes || shapes.length !== 1) return false
   const shape = shapes[0]
   return shape[0] === 0 && shape[1] === 0 && shape[2] === 0 && shape[3] === 1 && shape[4] === 1 && shape[5] === 1
 }
