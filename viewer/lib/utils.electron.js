eb863ff75bed54be360e6ef0a471d2a9610d9118
Karang <arthur.hennequin@free.fr>
2020-12-21 15:39:31 +0100

    Fix electron path when referenced from an other project

--- viewer/lib/utils.electron.js
+++ viewer/lib/utils.electron.js
@@ -2,12 +2,12 @@ const THREE = require('three')
 const path = require('path')
 
 function loadTexture (texture, cb) {
-  const url = path.resolve('../../public/' + texture)
+  const url = path.resolve(__dirname, '../../public/' + texture)
   cb(new THREE.TextureLoader().load(url))
 }
 
 function loadJSON (json, cb) {
-  cb(require('../../public/' + json))
+  cb(require(path.resolve(__dirname, '../../public/' + json)))
 }
 
 module.exports = { loadTexture, loadJSON }
