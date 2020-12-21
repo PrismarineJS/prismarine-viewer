245b2469c5407ea444d326424ecc1665824ed6bd
Karang <arthur.hennequin@free.fr>
2020-12-21 18:18:25 +0100

    Fix headless paths

--- viewer/lib/utils.js
+++ viewer/lib/utils.js
@@ -1,15 +1,16 @@
 
 const { loadImage } = require('node-canvas-webgl/lib')
 const THREE = require('three')
+const path = require('path')
 
 function loadTexture (texture, cb) {
-  loadImage(require.resolve('prismarine-viewer/public/' + texture)).then(image => {
+  loadImage(path.resolve(__dirname, '../../public/' + texture)).then(image => {
     cb(new THREE.CanvasTexture(image))
   })
 }
 
 function loadJSON (json, cb) {
-  cb(require('prismarine-viewer/public/' + json))
+  cb(require(path.resolve(__dirname, '../../public/' + json)))
 }
 
 module.exports = { loadTexture, loadJSON }
