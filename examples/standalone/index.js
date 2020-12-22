5bc8a4ebbe5f91fadaf7d558975c7077fe90e0b0
Romain Beaumont <romain.rom1@gmail.com>
2020-12-22 02:17:22 +0000

    remove / from standalone example to work in subdirs

--- examples/standalone/index.js
+++ examples/standalone/index.js
@@ -17,7 +17,7 @@ function getSchematic (url, cb) {
 }
 
 // load and paste a schematic
-getSchematic('/smallhouse1.schem', async (data) => {
+getSchematic('smallhouse1.schem', async (data) => {
   const schem = await Schematic.read(Buffer.from(data), '1.16.1')
 
   const version = schem.version
