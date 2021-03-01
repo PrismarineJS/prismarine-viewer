2efe02012ac08117351365130550fb421b6e0b14
Karang <arthur.hennequin@free.fr>
2021-03-02 00:04:29 +0100

    Copy textures instead of symlink

--- viewer/prerender.js
+++ viewer/prerender.js
@@ -2,7 +2,7 @@ const path = require('path')
 const { makeTextureAtlas } = require('./lib/atlas')
 const { prepareBlocksStates } = require('./lib/modelsBuilder')
 const mcAssets = require('minecraft-assets')
-const fs = require('fs')
+const fs = require('fs-extra')
 
 const texturesPath = path.resolve(__dirname, '../public/textures')
 if (!fs.existsSync(texturesPath)) {
@@ -25,9 +25,7 @@ for (const version of mcAssets.versions) {
   const blocksStates = JSON.stringify(prepareBlocksStates(assets, atlas))
   fs.writeFileSync(path.resolve(blockStatesPath, version + '.json'), blocksStates)
 
-  try {
-    fs.symlinkSync(assets.directory, path.resolve(texturesPath, version))
-  } catch {}
+  fs.copySync(assets.directory, path.resolve(texturesPath, version), { overwrite: true })
 }
 
 fs.writeFileSync(path.resolve(__dirname, '../public/supportedVersions.json'), '[' + mcAssets.versions.map(v => `"${v}"`).toString() + ']')
