709f6002dd399f0c290c1956e6cf50f55942cd60
Karang <arthur.hennequin@free.fr>
2021-03-01 23:40:37 +0100

    Add player model for bot

--- viewer/index.js
+++ viewer/index.js
@@ -2,5 +2,6 @@ module.exports = {
   Viewer: require('./lib/viewer').Viewer,
   WorldView: require('./lib/worldView').WorldView,
   MapControls: require('./lib/controls').MapControls,
+  Entity: require('./lib/entity/Entity'),
   getBufferFromStream: require('./lib/simpleUtils').getBufferFromStream
 }
