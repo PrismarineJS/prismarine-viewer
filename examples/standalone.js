8d3684eeafddc126fde11487bcc540c99348684c
Karang <arthur.hennequin@free.fr>
2021-04-18 15:22:34 +0200

    Fix standalone

--- examples/standalone.js
+++ examples/standalone.js
@@ -1,17 +1,34 @@
 const standaloneViewer = require('prismarine-viewer').standalone
 const { Vec3 } = require('vec3')
 
+const version = '1.13.2'
+
+const World = require('prismarine-world')(version)
+const Chunk = require('prismarine-chunk')(version)
+
 // Create a flat world with only 1 layer of stone at y=0
 function worldGenerator (x, y, z) {
   if (y > 0) return 0
   return 1
 }
 
-const viewer = standaloneViewer({ version: '1.13.2', generator: worldGenerator, center: new Vec3(0, 51, 0), port: 3000 })
+const world = new World((chunkX, chunkZ) => {
+  const chunk = new Chunk()
+  for (let y = 0; y < 256; y++) {
+    for (let x = 0; x < 16; x++) {
+      for (let z = 0; z < 16; z++) {
+        chunk.setBlockStateId(new Vec3(x, y, z), worldGenerator(chunkX * 16 + x, y, chunkZ * 16 + z))
+      }
+    }
+  }
+  return chunk
+})
+
+const viewer = standaloneViewer({ version, world, center: new Vec3(0, 51, 0), port: 3000 })
 
 async function menger (x, y, z, R) {
   if (R === 1) {
-    await viewer.world.setBlockStateId(new Vec3(x, y, z), 1)
+    await world.setBlockStateId(new Vec3(x, y, z), 1)
     return
   }
   R /= 3
