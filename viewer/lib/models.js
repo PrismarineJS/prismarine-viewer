423e387b838a9a70910f1962270c2e4b4dc30627
nova-27 <yutodes0305@gmail.com>
2023-03-05 18:26:51 +0900

    Fix lint errors

--- viewer/lib/models.js
+++ viewer/lib/models.js
@@ -466,11 +466,11 @@ function matchProperties (block, properties) {
 
   properties = parseProperties(properties)
   const blockProps = block.getProperties()
-  if (properties["OR"]) {
-    return properties["OR"].some((or) => matchProperties(block, or))
+  if (properties.OR) {
+    return properties.OR.some((or) => matchProperties(block, or))
   }
   for (const prop in blockProps) {
-    if (properties[prop] !== undefined && !properties[prop].split("|").some((value) => value == (blockProps[prop] + ''))) {
+    if (properties[prop] !== undefined && !properties[prop].split('|').some((value) => value === (blockProps[prop] + ''))) {
       return false
     }
   }
