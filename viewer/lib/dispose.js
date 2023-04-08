2215b1a9e9cd411136457694de12aa342d6a5f29
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-04-08 02:05:28 -0300

    Fix: RangeError maximum call stack size exceeded

--- viewer/lib/dispose.js
+++ viewer/lib/dispose.js
@@ -1,37 +1,72 @@
-const THREE = require('three')
+const THREE = require('three');  
+  
+/**  
+ * Recursively removes objects from the Three.js scene and disposes them from memory.  
+ * @param {Object} object - The object or group of objects to be removed and disposed from memory.
+ * @param {Set} visited - Set of objects already visited to prevent duplicate removals of objects.
+ */
+function dispose3(object, visited = new Set()) {  
+  try {  
+    if (visited.has(object)) return;  
+    visited.add(object);  
+  
+    if (object && typeof object === 'object') {  
+      if (Array.isArray(object)) {  
+        object.forEach(element => dispose3(element, visited));  
+      } else {  
+        disposeObject3D(object, visited);  
+      }  
+    }  
+  } catch (error) {  
+    console.log(error);  
+  }  
+}  
+  
+function disposeObject3D(object, visited) {  
+  if (object instanceof THREE.Object3D) {  
+    dispose3(object.geometry, visited);  
+    dispose3(object.material, visited);  
+    removeParent(object);  
+    dispose3(object.children, visited);  
+  } else {  
+    disposeBufferGeometry(object);  
+    disposeMaterial(object, visited);  
+    disposeOtherObjects(object, visited);  
+  }  
+}  
+  
+function removeParent(object) {  
+  if (object.parent) {  
+    object.parent.remove(object);  
+  }  
+}  
+  
+function disposeBufferGeometry(object) {  
+  if (object instanceof THREE.BufferGeometry) {  
+    object.dispose();  
+  }  
+}  
+  
+function disposeMaterial(object, visited) {  
+  if (object instanceof THREE.Material) {  
+    object.dispose();  
+    dispose3(object.materials, visited);  
+    dispose3(object.map, visited);  
+    dispose3(object.lightMap, visited);  
+    dispose3(object.bumpMap, visited);  
+    dispose3(object.normalMap, visited);  
+    dispose3(object.specularMap, visited);  
+    dispose3(object.envMap, visited);  
+  }  
+}  
+  
+function disposeOtherObjects(object, visited) {  
+  if (typeof object.dispose === 'function') {  
+    object.dispose();  
+  } else {  
+    Object.values(object).forEach(element => dispose3(element, visited));  
+  }  
+}  
+  
+module.exports = { dispose3 };  
 
-function dispose3 (o) {
-  try {
-    if (o && typeof o === 'object') {
-      if (Array.isArray(o)) {
-        o.forEach(dispose3)
-      } else if (o instanceof THREE.Object3D) {
-        dispose3(o.geometry)
-        dispose3(o.material)
-        if (o.parent) {
-          o.parent.remove(o)
-        }
-        dispose3(o.children)
-      } else if (o instanceof THREE.BufferGeometry) {
-        o.dispose()
-      } else if (o instanceof THREE.Material) {
-        o.dispose()
-        dispose3(o.materials)
-        dispose3(o.map)
-        dispose3(o.lightMap)
-        dispose3(o.bumpMap)
-        dispose3(o.normalMap)
-        dispose3(o.specularMap)
-        dispose3(o.envMap)
-      } else if (typeof o.dispose === 'function') {
-        o.dispose()
-      } else {
-        Object.values(o).forEach(dispose3)
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-module.exports = { dispose3 }
