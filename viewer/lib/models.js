87998eb9e7c5e2662fefb53659f166dec4a7c6a1
irkmandeer <37556625+irkmandeer@users.noreply.github.com>
2025-12-25 21:30:58 +0200

    Refactor air block condition in getModelVariants
    Quick fix for air check causing stairs to be not rendered.
    
    Mentioned in #427

--- viewer/lib/models.js
+++ viewer/lib/models.js
@@ -479,7 +479,7 @@ function matchProperties (block, properties) {
 
 function getModelVariants (block, blockStates) {
   // air, cave_air, void_air and so on...
-  if (block.name.includes('air')) return []
+  if (block.name == 'air' || block.name.endsWith('_air')) return []
   const state = blockStates[block.name] ?? blockStates.missing_texture
   if (!state) return []
   if (state.variants) {
